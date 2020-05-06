913cd79cc6adc1a5aa27fb8efd29770fee79c8b4
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-06 12:21:34 +0100

    added package-info for the project & corrected LICENSE.md to include my name

---
+++ src/main/java/com/qa/hwa/package-info.java
@@ -0,0 +1,25 @@
+package com.qa.hwa;
+/*
+* This package is the parent package of my hwa (Hobby Web App) project.
+* This application is intended to be a "Game Time Log" where users can perform CRUD functions on records of their game sessions.
+* It has been created by Caroline Strasenburgh as a QA Academy Trainee.
+*
+* MIT License
+* Copyright (c) 2020 Caroline Strasenburgh
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+* (the "Software"), to deal in the Software without restriction,
+* including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+* There should be a copy of this License in the main directory of this project @LICENSE.md
+* or a copy can be found at <https://opensource.org/licenses/MIT>
+*
+*/
