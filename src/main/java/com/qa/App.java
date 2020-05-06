d00f155e4263a296f899dba69d208d7519cd5299
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-06 11:32:01 +0100

    added @StringBootApplication so app runs as a SpringApplication

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -1,13 +1,16 @@
 package com.qa;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        SpringApplication.run(App.class);
     }
 }
