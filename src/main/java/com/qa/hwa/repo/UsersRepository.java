ae6b704692dda832062a8fc4f997de6b9b8dbe26
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-07 12:59:50 +0100

    added findUserByUsername() to UsersRepo

--- src/main/java/com/qa/hwa/repo/UsersRepository.java
+++ src/main/java/com/qa/hwa/repo/UsersRepository.java
@@ -4,6 +4,9 @@ import com.qa.hwa.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsersRepository extends JpaRepository<User, Long> {
+User findUserByUsername(String username);
 }
