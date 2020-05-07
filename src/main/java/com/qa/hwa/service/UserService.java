6596b6bb637c1c5e58642ca8369f698d64dbefc8
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-07 10:24:08 +0100

    added readAllUsers() in UserService and added @Autowired to constructor

--- src/main/java/com/qa/hwa/service/UserService.java
+++ src/main/java/com/qa/hwa/service/UserService.java
@@ -4,7 +4,11 @@ import com.qa.hwa.domain.User;
 import com.qa.hwa.dto.UserDTO;
 import com.qa.hwa.repo.UsersRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -13,6 +17,7 @@ public class UserService {
 
     public final ModelMapper mapper;
 
+    @Autowired
     public UserService(UsersRepository repo, ModelMapper mapper) {
         this.repo = repo;
         this.mapper = mapper;
@@ -20,5 +25,9 @@ public class UserService {
 
     private UserDTO mapToDTO(User user){
         return this.mapper.map(user, UserDTO.class);
+    }
+
+    public List<UserDTO> readAllUsers(){
+        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
     }
 }
