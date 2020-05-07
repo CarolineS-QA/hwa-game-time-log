818456919838bcca7303fa9d468573508c6076ae
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-07 10:29:05 +0100

    added UserController with /getAllUsers endpoint

---
+++ src/main/java/com/qa/hwa/rest/UserController.java
@@ -0,0 +1,26 @@
+package com.qa.hwa.rest;
+
+import com.qa.hwa.dto.UserDTO;
+import com.qa.hwa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<UserDTO>> getAllUsers(){
+        return ResponseEntity.ok(this.service.readAllUsers());
+    }
+}
