9846864e5acb3ebedee1ef3016ab63be45a337be
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-07 16:02:13 +0100

    added createUser() to UserController

--- src/main/java/com/qa/hwa/rest/UserController.java
+++ src/main/java/com/qa/hwa/rest/UserController.java
@@ -1,10 +1,14 @@
 package com.qa.hwa.rest;
 
+import com.qa.hwa.domain.User;
 import com.qa.hwa.dto.UserDTO;
 import com.qa.hwa.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +26,10 @@ public class UserController {
     @GetMapping("/getAllUsers")
     public ResponseEntity<List<UserDTO>> getAllUsers(){
         return ResponseEntity.ok(this.service.readAllUsers());
+    }
+
+    @PostMapping("/createUser")
+    public ResponseEntity<UserDTO> createUser(@RequestBody User user){
+        return new ResponseEntity<>(this.service.createUser(user), HttpStatus.CREATED);
     }
 }
