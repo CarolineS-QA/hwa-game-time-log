986296fe7fcef14272cd822e86420308e23bd0ab
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-08 20:26:01 +0100

    modified queries in GameSessionsRepository and added read gameSessions byUser and byTime to GameSessionsService

--- src/main/java/com/qa/hwa/service/GameSessionService.java
+++ src/main/java/com/qa/hwa/service/GameSessionService.java
@@ -1,12 +1,18 @@
 package com.qa.hwa.service;
 
 import com.qa.hwa.domain.GameSession;
+import com.qa.hwa.domain.User;
 import com.qa.hwa.dto.GameSessionDTO;
+import com.qa.hwa.dto.UserDTO;
 import com.qa.hwa.repo.GameSessionsRepository;
 import com.qa.hwa.repo.UsersRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GameSessionService {
@@ -28,7 +34,13 @@ public class GameSessionService {
         return this.mapper.map(session, GameSessionDTO.class);
     }
 
+    public List<GameSessionDTO> readAllSessionsByTimeOfSession(LocalDateTime time){
+        return this.sessionsRepo.findAllByTimeOfSessionOrderByTimeOfSessionDesc(time).stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
 
+    public List<GameSessionDTO> readAUsersGameSessions(User username){
+        return this.sessionsRepo.findAllByUsernameOrderByTimeOfSessionDesc(username).stream().map(this::mapToDTO).collect(Collectors.toList());
+    }
 
     public GameSessionDTO createGameSession(GameSession session){
         GameSession tempGameSession = this.sessionsRepo.save(session);
