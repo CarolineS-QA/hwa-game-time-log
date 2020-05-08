f1b74a76a4da58c4a30ae31c1d230ae4f7f273b2
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-08 23:24:38 +0100

    added test for readAUsersGameSessions and ignored new readAllSessionsByTimeOFSessionsTest in GameSessionServiceUnitTest - outlined issue in comment

--- src/test/java/com/qa/hwa/service/GameSessionServiceUnitTest.java
+++ src/test/java/com/qa/hwa/service/GameSessionServiceUnitTest.java
@@ -7,6 +7,7 @@ import com.qa.hwa.dto.UserDTO;
 import com.qa.hwa.repo.GameSessionsRepository;
 import com.qa.hwa.repo.UsersRepository;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
@@ -35,6 +37,7 @@ public class GameSessionServiceUnitTest {
     private ModelMapper mapper;
 
     private List<GameSession> gameSessionList;
+    private List<GameSessionDTO> gameSessionDTOList;
     private GameSession testSession;
     private Long sessionId = 1L;
     private GameSession testSessionWithId;
@@ -50,6 +53,7 @@ public class GameSessionServiceUnitTest {
     @Before
     public void setUp(){
         this.gameSessionList = new ArrayList<>();
+        this.gameSessionDTOList = new ArrayList<>();
         zeroTime = Duration.ofHours(0);
         player1 = new User(1L, "testUser", zeroTime, zeroTime, zeroTime, null);
         this.testSession = new GameSession(player1, "hello world", zeroTime, date);
@@ -57,6 +61,28 @@ public class GameSessionServiceUnitTest {
         this.testSessionWithId = new GameSession(testSession.getUsername(), testSession.getGameName(), testSession.getTimePlayed(), testSession.getTimeOfSession());
         this.testSessionWithId.setSessionId(sessionId);
         this.sessionDTO = this.mapToDTO(testSessionWithId);
+        this.gameSessionDTOList.add(sessionDTO);
+    }
+
+    // doesn't return anything because it findsByTimeOfSession before sorting
+    // -> looking for the exact time - so nothing gets picked up to sort!
+    // modify GameSessionRepo's FinaAll(), then GameSessionService - add the sorting in Service.
+    // - Controller should be the same as long a method name doesn't change
+    @Ignore
+    @Test
+    public void readAllSessionsByTimeOfSessionTest() {
+        when(repo.findAllByTimeOfSessionOrderByTimeOfSessionDesc(date)).thenReturn(this.gameSessionList);
+        when(this.mapper.map(testSessionWithId, GameSessionDTO.class)).thenReturn(sessionDTO);
+        assertEquals(this.service.readAllSessionsByTimeOfSession(), gameSessionDTOList);
+        verify(repo, times(1)).findAllByTimeOfSessionOrderByTimeOfSessionDesc(date);
+    }
+
+    @Test
+    public void readAUsersGameSessionsTest() {
+        when(this.repo.findAllByUsernameOrderByTimeOfSessionDesc(player1)).thenReturn(this.gameSessionList);
+        when(this.mapper.map(testSessionWithId, GameSessionDTO.class)).thenReturn(sessionDTO);
+        assertEquals(this.service.readAUsersGameSessions(player1), gameSessionDTOList);
+        verify(repo, times(1)).findAllByUsernameOrderByTimeOfSessionDesc(player1);
     }
 
     @Test
