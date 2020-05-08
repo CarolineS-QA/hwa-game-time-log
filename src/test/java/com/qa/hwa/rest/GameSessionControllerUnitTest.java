49c156b52d2e2b8464c84b751aba982827c750ef
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-08 16:22:18 +0100

    added GameSessionControllerUnitTest with createSession test

---
+++ src/test/java/com/qa/hwa/rest/GameSessionControllerUnitTest.java
@@ -0,0 +1,67 @@
+package com.qa.hwa.rest;
+
+import com.qa.hwa.domain.GameSession;
+import com.qa.hwa.domain.User;
+import com.qa.hwa.dto.GameSessionDTO;
+import com.qa.hwa.dto.UserDTO;
+import com.qa.hwa.service.GameSessionService;
+import com.qa.hwa.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GameSessionControllerUnitTest {
+    @InjectMocks
+    private GameSessionController sessionController;
+
+    @Mock
+    private GameSessionService service;
+
+    private User player1;
+    private List<GameSession> gameSessionsList;
+    private GameSession testSession;
+    private Long sessionId = 1L;
+    private GameSession testSessionWithId;
+    private GameSessionDTO sessionDTO;
+    private Duration zeroTime;
+    private LocalDateTime date;
+
+    private final ModelMapper mapper = new ModelMapper();
+
+    private GameSessionDTO mapToDTO(GameSession session){
+        return this.mapper.map(session, GameSessionDTO.class);
+    }
+
+    @Before
+    public void setUp(){
+        gameSessionsList = new ArrayList<>();
+        this.player1 = new User("testUser", zeroTime, zeroTime, zeroTime, null);
+        this.testSession = new GameSession(player1, "hello world", zeroTime, date);
+        this.gameSessionsList.add(testSession);
+        this.testSessionWithId = new GameSession(testSession.getUsername(), testSession.getGameName(), testSession.getTimePlayed(), testSession.getTimeOfSession());
+        this.testSessionWithId.setSessionId(sessionId);
+        this.sessionDTO = this.mapToDTO(testSessionWithId);
+    }
+
+    @Test
+    public void createGameSessionTest() {
+        when(this.service.createGameSession(testSession)).thenReturn(this.sessionDTO);
+        assertEquals(this.sessionController.createGameSession(testSession), new ResponseEntity<>(this.sessionDTO, HttpStatus.CREATED));
+        verify(this.service, times(1)).createGameSession(testSession);
+    }
+}
