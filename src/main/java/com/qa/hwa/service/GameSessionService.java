6f97312c26b3cffa974cba2aef6b84a907b71b61
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-08 11:36:02 +0100

    added GameSessionService

---
+++ src/main/java/com/qa/hwa/service/GameSessionService.java
@@ -0,0 +1,26 @@
+package com.qa.hwa.service;
+
+import com.qa.hwa.domain.GameSession;
+import com.qa.hwa.dto.GameSessionDTO;
+import com.qa.hwa.repo.GameSessionsRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameSessionService {
+
+    public final GameSessionsRepository repo;
+
+    public final ModelMapper mapper;
+
+    @Autowired
+    public GameSessionService(GameSessionsRepository repo, ModelMapper mapper) {
+        this.repo = repo;
+        this.mapper = mapper;
+    }
+
+    private GameSessionDTO mapToDTO(GameSession session){
+        return this.mapper.map(session, GameSessionDTO.class);
+    }
+}
