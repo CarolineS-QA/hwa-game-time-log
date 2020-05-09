bad711d3a5e95cdae006b637ea5030a82de1de8f
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-09 12:05:39 +0100

    added updateGameSession to GSService, with new GameSessionNotFoundException

--- src/main/java/com/qa/hwa/service/GameSessionService.java
+++ src/main/java/com/qa/hwa/service/GameSessionService.java
@@ -4,6 +4,8 @@ import com.qa.hwa.domain.GameSession;
 import com.qa.hwa.domain.User;
 import com.qa.hwa.dto.GameSessionDTO;
 import com.qa.hwa.dto.UserDTO;
+import com.qa.hwa.exceptions.GameSessionNotFoundException;
+import com.qa.hwa.exceptions.UserNotFoundException;
 import com.qa.hwa.repo.GameSessionsRepository;
 import com.qa.hwa.repo.UsersRepository;
 import org.modelmapper.ModelMapper;
@@ -11,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,6 +52,17 @@ public class GameSessionService {
 
     public GameSessionDTO createGameSession(GameSession session){
         GameSession tempGameSession = this.sessionsRepo.save(session);
+        return this.mapToDTO(tempGameSession);
+    }
+
+    //Restricting the Update. So a session can't be reassigned to different user.
+    public GameSessionDTO updateGameSession(Long id, @NotNull GameSession session){
+        GameSession update = this.sessionsRepo.findById(id).orElseThrow(GameSessionNotFoundException::new);
+        update.setGameName(session.getGameName());
+        update.setTimeOfSession(session.getTimeOfSession()); //could set this to LocalDateTime.now()
+        update.setTimePlayed(session.getTimePlayed());
+        //update.setUsername(session.getUsername());
+        GameSession tempGameSession = this.sessionsRepo.save(update);
         return this.mapToDTO(tempGameSession);
     }
 }
