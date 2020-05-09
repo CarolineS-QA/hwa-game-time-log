87dc9b43a0db40dc44692c313f7c6090dd139f55
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-09 11:49:23 +0100

    modified comments in App - need to add shutdown for springApp / Java (end of main method)

--- src/main/java/com/qa/hwa/App.java
+++ src/main/java/com/qa/hwa/App.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
- *
+ * Add bean definitions here?
+ * .close() of SpringApplication - need to set conditions...
  */
 @SpringBootApplication
 public class App 
 {
     public static void main( String[] args ){
         SpringApplication.run(App.class);
+        //add instance of appContext here....
+        //conditions for shutdown?
+        //SpringApplication.exit(appContext, 0);
     }
 }
