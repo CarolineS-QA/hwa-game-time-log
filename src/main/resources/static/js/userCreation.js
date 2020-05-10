2918f95c14623e48e7b3c48bacdabb1f074babc2
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-10 23:46:57 +0100

    modified freeTime to valueAsNumber in userCreation.js, added a redirect to users page

--- src/main/resources/static/js/userCreation.js
+++ src/main/resources/static/js/userCreation.js
@@ -3,9 +3,8 @@ const REQ = new XMLHttpRequest();
 
 function postNewUser() {
     let username = document.getElementById("username").value;
-    let freeTimeHours = document.getElementById("freeTimeHours").value;
-    let freeTimeMinutes = document.getElementById("freeTimeMinutes").value;
-
+    let freeTimeHours = document.getElementById("freeTimeHours").valueAsNumber;
+    let freeTimeMinutes = document.getElementById("freeTimeMinutes").valueAsNumber;
 
     let freeTime = freeTimeMinutes + (freeTimeHours * 60);
 
@@ -26,14 +25,18 @@ function postNewUser() {
             console.log(REQ.response);
             console.log("The data has been sent.");
             window.alert("A User has been created!")
+            window.location.replace("./Users.html");
         } else {
             console.log(REQ);
             console.log(REQ.response);
             console.log(`Oh no! You should handle the Error(s)!`);
             window.alert("Oops! Something went wrong...")
             console.log(freeTimeMinutes);
+            console.log(freeTimeMinutes.data_type);
             console.log(freeTimeHours);
+            console.log(freeTimeHours.data_type);
             console.log(freeTime);
+            console.log(freeTime.data_type);
             console.log(jsonString);
 
         }
@@ -45,7 +48,7 @@ function postNewUser() {
 submitCreateUser.addEventListener('click', function (event){
     event.preventDefault();
     postNewUser();
-    let form = document.getElementById("userDetails");
-    let section = document.getElementsByTagName("section");
-    section.removeChild(form);
+    // let form = document.getElementById("userDetails");
+    // let section = document.getElementsByTagName("section");
+    // section.removeChild(form);
 })
