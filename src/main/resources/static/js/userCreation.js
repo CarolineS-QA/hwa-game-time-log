3cf42cc4ca4318da2d0d33588870ca78717d104e
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-10 22:43:30 +0100

    added API call for /createUser in userCreation.js

---
+++ src/main/resources/static/js/userCreation.js
@@ -0,0 +1,38 @@
+let submitCreateUser = document.querySelector("#bSubmit");
+const REQ = new XMLHttpRequest();
+
+function postNewUser() {
+    let username = document.getElementById("username").value;
+    let freeTimeHours = document.getElementById("freeTimeHours").value;
+    let freeTimeMinutes = document.getElementById("freeTimeMinutes").value;
+
+    let freeTime = freeTimeMinutes + (freeTimeHours * 60);
+
+    let jsonString = JSON.stringify({
+        "username": username,
+        "totalTimePlayed": 0,
+        "freeTime": freeTime,
+        "timeRemaining": freeTime
+        });
+
+    REQ.open('POST', '/createUser');
+    REQ.setRequestHeader('Content-Type', 'Application/json');
+    REQ.setRequestHeader('Access-Control-Allow-Origin', '*');
+    REQ.onload = () => {
+        if (REQ.status === 201 && REQ.readyState === 4) {
+            console.log(REQ);
+            console.log(REQ.response);
+            console.log("The data has been sent.");
+            window.alert("A User has been created!")
+        } else {
+            console.log(REQ);
+            console.log(REQ.response);
+            console.log(`Oh no! You should handle the Error(s)!`);
+            window.alert("Oops! Something went wrong...")
+        }
+    }
+    REQ.send(jsonString);
+}
+
+
+submitCreateUser.addEventListener('click', postNewUser)
