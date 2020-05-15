791001d04f9976e1840ffabceb252e3a0ee6ee81
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-15 14:27:07 +0100

    added userId requirement on GameSessions.html form to create a GameSession - removes the need for get request in gameSessionCRUD.js

--- src/main/resources/static/js/gameSessionCRUD.js
+++ src/main/resources/static/js/gameSessionCRUD.js
@@ -4,8 +4,8 @@ const REQ = new XMLHttpRequest();
 let submitCreateGameSession = document.querySelector("#bSubmitGameSession");
 
 function postNewGameSession() {
-    let user = REQ.response;
-    // might only need userId and username...
+    let gamerId = document.getElementById("gamerId").valueAsNumber;
+    let gamerName = document.getElementById("gamerName").value;
     let gameName = document.getElementById("gameName").value;
     let timePlayedHours = document.getElementById("timePlayedHours").valueAsNumber;
     let timePlayedMinutes = document.getElementById("timePlayedMinutes").valueAsNumber;
@@ -13,10 +13,14 @@ function postNewGameSession() {
     let timePlayed = Number(timePlayedMinutes) + (Number(timePlayedHours) * 60);
     let timeOfSession = "2007-12-03T10:15:30" //Date.now().toString();
 
+    //only userId and username are needed for user, other values can be added but they don't affect the User
     let jsonString = JSON.stringify(
         {
             "gameName": gameName,
-            "user": user,
+            "user": {
+                "userId": gamerId,
+                "username": gamerName
+            },
             "timePlayed": timePlayed,
             "timeOfSession": timeOfSession,
         });
@@ -33,6 +37,8 @@ function postNewGameSession() {
             window.alert("A Game Session has been created!");
             console.log(Date.now().toString())
             window.location.reload();
+        } else if (REQ.status === 405) {
+            window.alert("Bad request! Please check the data you are inputting.");
         } else {
             console.log(REQ);
             console.log(REQ.response);
@@ -47,31 +53,7 @@ function postNewGameSession() {
     REQ.send(jsonString);
 }
 
-function getYourUser() {
-    let username = document.getElementById("gamerId").value;
-    REQ.onload = () => {
-        if (REQ.status === 200 && REQ.readyState === 4) {
-            console.log(REQ.response);
-            postNewGameSession(REQ.response);
-            console.log("The request for data has been sent.");
-        } else if (REQ.status === 404) {
-            window.alert("Your user wasn't found! Error 404.")
-        } else {
-            console.log(REQ);
-            console.log(REQ.response);
-            console.log(`Oh no! You should handle the Error(s)!`);
-            window.alert("Oops! Something went wrong...")
-        }
-    }
-    REQ.open('GET', `/getUserByUsername/${username}`);
-    REQ.setRequestHeader('Content-Type', 'Application/json');
-    REQ.setRequestHeader('Access-Control-Allow-Origin', '*');
-    REQ.responseType = 'json';
-    REQ.send();
-}
-
 submitCreateGameSession.addEventListener('click', function (event) {
     event.preventDefault();
-    getYourUser();
-    //postNewGameSession();
+    postNewGameSession();
 })
