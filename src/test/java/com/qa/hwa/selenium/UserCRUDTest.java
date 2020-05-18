35a6f682e3c547e3101418da0b117da1286a91dc
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-05-18 12:28:19 +0100

    added Selenium test file UserCRUDTest, set up with extent-reports

---
+++ src/test/java/com/qa/hwa/selenium/UserCRUDTest.java
@@ -0,0 +1,56 @@
+package com.qa.hwa.selenium;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.io.File;
+
+public class UserCRUDTest {
+    WebDriver driver;
+    ExtentReports report;
+    ExtentTest test;
+
+    @BeforeTest
+    public void startReport(){
+        report = new ExtentReports(
+                System.getProperty("user.dir") + "/test-output/Report.html",
+                true
+        );
+        report
+                .addSystemInfo("Host Name", "QA")
+                .addSystemInfo("Tester", "Caroline");
+        report.loadConfig(new File(System.getProperty("user.dir") + "\\extent-report.xml"));
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void getResult(ITestResult result){
+        driver.close();
+        if(result.getStatus() == ITestResult.FAILURE){
+            test.log(LogStatus.FAIL, "Test has failed " + result.getName());
+            test.log(LogStatus.FAIL, "Test has failed " + result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(LogStatus.PASS, "Test has passed " + result.getName());
+        }
+        report.endTest(test);
+    }
+
+    @AfterTest
+    public void endReport(){
+        report.flush();
+        report.close();
+    }
+
+}
