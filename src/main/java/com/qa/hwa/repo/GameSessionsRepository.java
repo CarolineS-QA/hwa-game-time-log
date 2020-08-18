1270bab567ba5c7839f8d26f1d143f7335a7d6f2
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-18 13:58:24 +0100

    modified Repos to extend MongoDB instead of JPA

--- src/main/java/com/qa/hwa/repo/GameSessionsRepository.java
+++ src/main/java/com/qa/hwa/repo/GameSessionsRepository.java
@@ -3,13 +3,13 @@ package com.qa.hwa.repo;
 import com.qa.hwa.domain.GameSession;
 import com.qa.hwa.domain.User;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface GameSessionsRepository extends JpaRepository<GameSession, Long> {
+public interface GameSessionsRepository extends MongoRepository<GameSession, Long> {
     List<GameSession> findAllByUser(User username);
     List<GameSession> findAll(Sort sort);
 }
--- src/main/java/com/qa/hwa/repo/UsersRepository.java
+++ src/main/java/com/qa/hwa/repo/UsersRepository.java
@@ -1,10 +1,10 @@
 package com.qa.hwa.repo;
 
 import com.qa.hwa.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<User, Long> {
+public interface UsersRepository extends MongoRepository<User, Long> {
 User findUserByUsername(String username);
 }
