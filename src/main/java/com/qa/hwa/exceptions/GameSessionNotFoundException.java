2fa7b15ad565a565a01a58a4aa410a3ff2c7be25
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-18 14:00:03 +0100

    modified exceptions - FileNF instead of EntityNF

--- src/main/java/com/qa/hwa/exceptions/GameSessionNotFoundException.java
+++ src/main/java/com/qa/hwa/exceptions/GameSessionNotFoundException.java
@@ -3,8 +3,10 @@ package com.qa.hwa.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.persistence.EntityNotFoundException;
+// I changed from EntityNotFound to FileNotFound
+// import javax.persistence.EntityNotFoundException;
+import java.io.FileNotFoundException;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The game session doesn't exist")
-public class GameSessionNotFoundException extends EntityNotFoundException {
+public class GameSessionNotFoundException extends FileNotFoundException {
 }
--- src/main/java/com/qa/hwa/exceptions/UserNotFoundException.java
+++ src/main/java/com/qa/hwa/exceptions/UserNotFoundException.java
@@ -3,8 +3,10 @@ package com.qa.hwa.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.persistence.EntityNotFoundException;
+// changed from EntityNotFound to FileNotFound
+// import javax.persistence.EntityNotFoundException;
+import java.io.FileNotFoundException;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The user doesn't exist")
-public class UserNotFoundException extends EntityNotFoundException {
+public class UserNotFoundException extends FileNotFoundException {
 }
